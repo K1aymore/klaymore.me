df5ca724ced478543ad1665ee8babd1b3ae9b98a
Klaymore <klaymorer@protonmail.com>
2021-11-30 13:24:13 -0800

    php adding comments works, images work with php. Comment form is too centered. Needs some ui design

---
+++ comic/01/01/addComment.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$str = "\n".$_GET['username'].":\n".$_GET['comment']."\n";
+echo file_put_contents("comments.txt", $str, FILE_APPEND | LOCK_EX);
+
+?>
+
+</body>
+</html> 
