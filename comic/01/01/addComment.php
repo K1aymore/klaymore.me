d3f2996e35f8e7c3060ab6a68e462266dfc62bb1
Klaymore <klaymorer@protonmail.com>
2021-11-30 17:27:12 -0800

    Comments work with php. Changed design to light green links and pure black background. Indented About page.

--- comic/01/01/addComment.php
+++ comic/01/01/addComment.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$str = "\n".$_GET['username'].":\n".$_GET['comment']."\n";
+$str = "\n".$_GET['username'].":\n   ".$_GET['comment']."\n";
 echo file_put_contents("comments.txt", $str, FILE_APPEND | LOCK_EX);
 
 ?>
