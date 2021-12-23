d8d98d6ebbbce2c961d29d8e19853295049feb12
Klaymore <klaymorer@protonmail.com>
2021-12-23 13:43:28 -0800

    addComment.php should only add comment if there's actually a username and comment

--- src/comic/addComment.php
+++ src/comic/addComment.php
@@ -4,10 +4,12 @@
   $str = "\n".htmlentities($_POST['username']).":\n&emsp;&emsp;". htmlentities($_POST['comment'])." {.comments}\n";
   
   $file = "/synced/Websites/klaymore.me/src/comic/".$_POST['chapter']."/".$_POST['page']."/index.md";
-  
-  $myfile = fopen($file, "a") or die("Unable to open file!");
-  fwrite($myfile, $str);
-  fclose($myfile);
-  
+
+  if (isset($_POST['username']) && $_POST['username'] !== '' && isset($_POST['comment']) && $_POST['comment'] !== '') {
+    $myfile = fopen($file, "a") or die("Unable to open file!");
+    fwrite($myfile, $str);
+    fclose($myfile);
+  }
+
   exit;
 ?>
