f5fc4429499f3d5605f9350dbe28c0c8896cc61d
Klaymore <klaymorer@protonmail.com>
2021-12-27 21:50:27 -0800

    CSS and mobile.js now get imported into every HTML document, so it works without the other files.

--- src/css/mobile.js
+++
@@ -1,7 +0,0 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  var link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.type = 'text/css';
-  link.href = '/css/mobile.css';
-  document.getElementsByTagName('HEAD')[0].appendChild(link);
-}
