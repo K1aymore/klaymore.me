9205177e70ddee1a49f860124dfda417434f94bd
K1aymore <klaymorer@protonmail.com>
2022-04-29 15:38:12 -0700

    Fixed adding comments

--- src/addCommentRxse.php
+++ src/addCommentRxse.php
@@ -3,7 +3,7 @@
 
   $str = "\n".htmlentities($_POST['username']).":\n&emsp;&emsp;". htmlentities($_POST['comment'])." {.comments}\n";
 
-  $file = "../src/Rxse/".$_POST['comic']."/".$_POST['chapter']."/".$_POST['page']."/index.md";
+  $file = "../src/Rxse/comics/".$_POST['comic']."/".$_POST['chapter']."/".$_POST['page']."/index.md";
 
   if (isset($_POST['username']) && $_POST['username'] !== '' && isset($_POST['comment']) && $_POST['comment'] !== '') {
     $myfile = fopen($file, "a") or die("Unable to open file!");
